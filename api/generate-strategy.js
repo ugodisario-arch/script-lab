44ad07f616edeb833cd7c5f61a1e49782dd01719
ugodisario-arch <ugodisario@gmail.com>
2025-10-15 11:34:11 +0200

    Update generate-strategy.js

--- api/generate-strategy.js
+++ api/generate-strategy.js
@@ -43,7 +43,7 @@ export default async function handler(req, res) {
           content: prompt
         }
       ],
-      model: 'mixtral-8x7b-32768',
+      model: 'llama-3.3-70b-versatile',
       temperature: 0.7,
       max_tokens: 3000,
     });
